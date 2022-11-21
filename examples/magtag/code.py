68131ce8c054d7ff869a821e46ab9ad8fbe5a1d9
Keith Schincke <keith.schincke+github@gmail.com>
2022-11-21 15:26:48 -0600

    Update magtag/code.py to remove old code

--- examples/magtag/code.py
+++ examples/magtag/code.py
@@ -11,7 +11,6 @@ Various Adafruit howto's, API documentation and *gasp* looking at various Adafru
 All mistakes are my fault.
 '''
 
-#from kds_analogclock.analogclock import AnalogClock
 from magtag_analogclock.magtag_analogclock import Magtag_AnalogClock
 
 def main():
